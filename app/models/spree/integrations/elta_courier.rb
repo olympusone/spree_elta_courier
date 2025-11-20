45f8e680f8033a3e88459224f2e1a49c57a569c0
DimiDev <dimidev@olympusone.com>
2025-11-20 17:02:41 +0200

    Refactor Elta Courier integration: remove unused controller, implement voucher creation service, update form fields and localization keys

--- app/models/spree/integrations/elta_courier.rb
+++ app/models/spree/integrations/elta_courier.rb
@@ -5,13 +5,13 @@ module Spree
       preference :customer_code, :string
       preference :user_code, :string
       preference :password, :string
-      preference :paper_type, :string, default: "1"
+      preference :paper_size, :string, default: "1"
 
       validates :preferred_wsdl_url, presence: true
       validates :preferred_customer_code, presence: true
       validates :preferred_user_code, presence: true
       validates :preferred_password, presence: true
-      validates :preferred_paper_type, presence: true, inclusion: { in: %w[0 1] }
+      validates :preferred_paper_size, presence: true, inclusion: { in: %w[0 1] }
 
       def self.integration_group
         'shipping'
---
+++ app/services/spree_elta_courier/create_voucher.rb
@@ -0,0 +1,42 @@
+module SpreeEltaCourier
+  class CreateVoucher
+    attr_reader :order, :shipment, :client
+
+    def initialize(shipment)
+      @order = shipment.order
+      @shipment = shipment
+
+      @client = EltaCourier::SoapClient.new(:create)
+    end
+
+    def call
+      address = shipment.address
+      cod_payment = order.payment_method&.cod_payment?
+
+      record = {
+        'pel_paral_name' => address.full_name,
+        'pel_paral_address' => address.address1,
+        'pel_paral_area' => address.city,
+        'pel_paral_tk' => address.zipcode.gsub(/\s+/, ''),
+        'pel_paral_thl_1' => address.phone,
+        'pel_baros' => shipment.item_weight.to_f,
+        'pel_temaxia' => 1,
+        'pel_paral_sxolia' => order.special_instructions,
+        'pel_ant_poso' => cod_payment ? shipment.final_price_with_items.to_f : 0,
+        'pel_ref_no' => shipment.number,
+        'sideta_eidos' => 2 # 1=Documents, 2=Parcel
+      }
+
+      result = client.call(
+        :read,
+        record
+      )
+
+      return result if result[:st_flag].to_i.zero?
+
+      raise VoucherError, "Failed to create voucher: #{result[:st_title]}" if result[:st_flag].to_i != 0
+    end
+
+    class VoucherError < StandardError; end
+  end
+end
